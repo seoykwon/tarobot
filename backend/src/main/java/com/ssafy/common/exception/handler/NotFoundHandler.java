421ac18bfeece77b5a4473551a8a98089b487947
민경훈 <mgh123gh@naver.com>
2025-01-21 14:41:24 +0900

    indiv

--- backend/src/main/java/com/ssafy/common/exception/handler/NotFoundHandler.java
+++ backend/src/main/java/com/ssafy/common/exception/handler/NotFoundHandler.java
@@ -27,24 +27,24 @@ import org.springframework.web.servlet.NoHandlerFoundException;
 
 @ControllerAdvice
 public class NotFoundHandler {
-	@Value("${spa.default-file}")
-	String defaultFile;
-	 
+//	@Value("${spa.default-file}")
+//	String defaultFile;
+
 	@ExceptionHandler(NoHandlerFoundException.class)
 	public ResponseEntity<String> renderDefaultPage(NoHandlerFoundException ex) {
 		String url = ex.getRequestURL();
 		if(url.startsWith("/api/")) {
 			return ResponseEntity.notFound().build();
 		}else {
-			try {
-				ClassPathResource classPathResource = new ClassPathResource(defaultFile);
-				InputStream inputStream = classPathResource.getInputStream();
-    				String body = StreamUtils.copyToString(inputStream, Charset.defaultCharset());
-			    return ResponseEntity.ok().contentType(MediaType.TEXT_HTML).body(body);
-			} catch (IOException e) {
-				e.printStackTrace();
+//			try {
+//				ClassPathResource classPathResource = new ClassPathResource(defaultFile);
+//				InputStream inputStream = classPathResource.getInputStream();
+//    				String body = StreamUtils.copyToString(inputStream, Charset.defaultCharset());
+//			    return ResponseEntity.ok().contentType(MediaType.TEXT_HTML).body(body);
+//			} catch (IOException e) {
+//				e.printStackTrace();
 				return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("There was an error completing the action.");
-			}
+//			}
 		}
 	}
 }
---
+++ frontend/src/app/community/page.tsx
@@ -0,0 +1,10 @@
+const CoummunityPage = () => {
+  return (
+    <div className="min-h-screen pb-16">
+      <h1 className="p-4 text-2xl font-bold">커뮤니티</h1>
+    </div>
+  )
+}
+
+export default CoummunityPage
+  
