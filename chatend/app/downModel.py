e2919fbbaf7fa9238c6afb54189b4be71ed9389b
민경훈 <mgh123gh@naver.com>
2025-01-22 16:24:21 +0900

    chatend

---
+++ chatend/app/downModel.py
@@ -0,0 +1,13 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+# 모델과 토크나이저를 로컬 디렉터리에 저장
+model_name = "TinyLlama/TinyLlama-1.1B-intermediate-step-715k-1.5T"
+local_dir = f"./models/{model_name.replace('/', '_')}"
+
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+tokenizer.save_pretrained(local_dir)
+
+model = AutoModelForCausalLM.from_pretrained(model_name)
+model.save_pretrained(local_dir)
+
+print(f"Model and tokenizer saved to {local_dir}")
