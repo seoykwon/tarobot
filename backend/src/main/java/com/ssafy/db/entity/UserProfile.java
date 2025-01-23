377f7fb178b8c2ba7321c87c4d3720a595ca832b
민경훈 <mgh123gh@naver.com>
2025-01-23 09:03:18 +0900

    feat(UserProfile): 유저 프로필 기능 구현 유저 생성 과정에서 nullable = false인 name 값을 받지 않는 문제를 해결하고 userProfile의 정상적인 생성을 확인하였습니다. UserServiceImpl, UserProfile

--- backend/src/main/java/com/ssafy/db/entity/UserProfile.java
+++ backend/src/main/java/com/ssafy/db/entity/UserProfile.java
@@ -4,15 +4,12 @@ package com.ssafy.db.entity;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -21,7 +18,6 @@ public class UserProfile extends BaseEntity {
 
     @OneToOne
     @JoinColumn(name = "user_id", nullable = false, unique = true)
-    @JsonIgnore
     private User user;
 
     @Column(nullable = false, unique = true)
