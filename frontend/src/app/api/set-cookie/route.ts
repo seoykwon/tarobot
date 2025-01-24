9e1998facbfc8f38c21c140a2f269d13e50a0a81
Riis <gunil099@gmail.com>
2025-01-24 13:15:46 +0900

    fix: 첫방문 페이지 쿠키 오작동 수정 쿠키가 저장되지 않는 문제를 수정

--- frontend/src/app/api/set-cookie/route.ts
+++ frontend/src/app/api/set-cookie/route.ts
@@ -1,15 +1,15 @@
 // app/api/set-cookie/route.ts
 import { NextResponse } from 'next/server';
 
-export async function GET() {
-  const response = NextResponse.json({ message: 'Cookie has been set' });
+export async function POST(request: Request) {
+  const response = NextResponse.redirect(new URL('/home', request.url)); // 절대 URL 사용
 
-  // 쿠키 설정
-  response.cookies.set('hasVisited', 'true', {
-    maxAge: 60 * 60 * 24 * 365, // 1년 (초 단위)
-    path: '/', // 모든 경로에서 유효
+  // 쿠키 설정 (HttpOnly, Secure 옵션 포함)
+  response.cookies.set('isVisited', 'true', {
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
+    secure: process.env.NODE_ENV === 'production', // 프로덕션 환경에서만 Secure 설정
+    path: '/',
+    maxAge: 60 * 60 * 24 * 7, // 1주일 동안 유효
   });
 
   return response;
