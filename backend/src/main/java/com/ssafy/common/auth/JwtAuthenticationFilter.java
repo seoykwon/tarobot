d0cea297bccfc014df664af19ec66fbd1e3eec0e
민경훈 <mgh123rg@gmail.com>
2025-01-29 14:26:41 +0900

    feat(Auth): JWT기반 유저 로그인

--- backend/src/main/java/com/ssafy/common/auth/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/ssafy/common/auth/JwtAuthenticationFilter.java
@@ -45,17 +45,17 @@ public class JwtAuthenticationFilter extends BasicAuthenticationFilter {
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
-		// Read the Authorization header, where the JWT Token should be
+		// JWT 토큰이 있는 요청 헤더의 Authorization 키 값 조회
         String header = request.getHeader(JwtTokenUtil.HEADER_STRING);
 
-        // If header does not contain BEARER or is null delegate to Spring impl and exit
+        // 헤더에 Bearer가 없거나 null이면 exit
         if (header == null || !header.startsWith(JwtTokenUtil.TOKEN_PREFIX)) {
             filterChain.doFilter(request, response);
             return;
         }
         
         try {
-            // If header is present, try grab user principal from database and perform authorization
+            // 헤더가 있으면 유저 정보를 DB에서 가져오고 인가 수행
             Authentication authentication = getAuthentication(request);
             // jwt 토큰으로 부터 획득한 인증 정보(authentication) 설정.
             SecurityContextHolder.getContext().setAuthentication(authentication);
@@ -72,14 +72,14 @@ public class JwtAuthenticationFilter extends BasicAuthenticationFilter {
         String token = request.getHeader(JwtTokenUtil.HEADER_STRING);
         // 요청 헤더에 Authorization 키값에 jwt 토큰이 포함된 경우에만, 토큰 검증 및 인증 처리 로직 실행.
         if (token != null) {
-            // parse the token and validate it (decode)
+            // 토큰 유효성 검사 (decode)
             JWTVerifier verifier = JwtTokenUtil.getVerifier();
             JwtTokenUtil.handleError(token);
             DecodedJWT decodedJWT = verifier.verify(token.replace(JwtTokenUtil.TOKEN_PREFIX, ""));
             String userId = decodedJWT.getSubject();
             
-            // Search in the DB if we find the user by token subject (username)
-            // If so, then grab user details and create spring auth token using username, pass, authorities/roles
+            // DB에서 토큰의 속성으로 user 정보를 가져올 수 있는 지 체크 (username)
+            // 그렇다면 user 정보를 가져오고 스프링 인증 토큰 생성 using username, pass, authorities/roles
             if (userId != null) {
                     // jwt 토큰에 포함된 계정 정보(userId) 통해 실제 디비에 해당 정보의 계정이 있는지 조회.
             		User user = userService.getUserByUserId(userId);
@@ -88,7 +88,7 @@ public class JwtAuthenticationFilter extends BasicAuthenticationFilter {
                 		SsafyUserDetails userDetails = new SsafyUserDetails(user);
                 		UsernamePasswordAuthenticationToken jwtAuthentication = new UsernamePasswordAuthenticationToken(userId,
                 				null, userDetails.getAuthorities());
-                		jwtAuthentication.setDetails(userDetails);
+                    jwtAuthentication.setDetails(userDetails);
                 		return jwtAuthentication;
                 }
             }
