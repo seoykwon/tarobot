7b2d4edfdea5095e40da34c41df2b1a2bac2cf54
KmingSong <eclatantsum@gmail.com>
2025-02-06 12:46:44 +0900

    벚꽃 화면 테두리에 붙어서 시작하는 거 수정

--- frontend/src/components/CherryBlossomBackground.tsx
+++ frontend/src/components/CherryBlossomBackground.tsx
@@ -6,17 +6,15 @@ import styles from "./CherryBlossomBackground.module.css"
 const CherryBlossomBackground: React.FC = () => {
   const petals = React.useMemo(() => {
     return [...Array(33)].map((_, i) => {
-      const isFromTop = Math.random() > 0.3 // 70% chance to fall from top
-      const angle = Math.random() * 30 + 15 // Random angle between 15 and 45 degrees
+      const startFromRight = Math.random() > 0.7 // 30% chance to start from right
       return {
         key: i,
         style: {
-          right: isFromTop ? `${Math.random() * 100}%` : "0px",
-          top: isFromTop ? "0px" : `${Math.random() * 100}%`,
+          right: startFromRight ? `${Math.random() * 20}%` : `${Math.random() * 100}%`,
+          top: startFromRight ? `${Math.random() * 100}%` : `${Math.random() * 20 - 10}%`,
           animationDuration: `${10 + Math.random() * 5}s`,
           animationDelay: `${Math.random() * 5}s`,
-          transform: `rotate(${Math.random() * 360}deg)`,
-          "--falling-angle": `${angle}deg`,
+          opacity: 0,
         } as React.CSSProperties,
       }
     })
