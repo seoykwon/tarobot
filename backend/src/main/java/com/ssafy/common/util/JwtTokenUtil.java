a5d0fb33ddbda429e28764ef4427f2005f464316
yeahhh1e <cakejuuu@gmail.com>
2025-02-18 14:13:31 +0900

    refactor: 주석 제거

--- backend/src/main/java/com/ssafy/common/util/JwtTokenUtil.java
+++ backend/src/main/java/com/ssafy/common/util/JwtTokenUtil.java
@@ -24,26 +24,27 @@ public class JwtTokenUtil {
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
     public static final String ISSUER = "ssafy.com";
-    
-    @Autowired
-	public JwtTokenUtil(@Value("${jwt.secret}") String secretKey, @Value("${jwt.expiration}") Integer expirationTime) {
-		this.secretKey = secretKey;
-		this.expirationTime = expirationTime;
-	}
-    
-	public void setExpirationTime() {
-    		JwtTokenUtil.expirationTime = expirationTime;
-	}
 
-	public static JWTVerifier getVerifier() {
+    @Autowired
+    public JwtTokenUtil(@Value("${jwt.secret}") String secretKey, @Value("${jwt.expiration}") Integer expirationTime) {
+        this.secretKey = secretKey;
+        this.expirationTime = expirationTime;
+    }
+
+    public void setExpirationTime() {
+        //JwtTokenUtil.expirationTime = Integer.parseInt(expirationTime);
+        JwtTokenUtil.expirationTime = expirationTime;
+    }
+
+    public static JWTVerifier getVerifier() {
         return JWT
                 .require(Algorithm.HMAC512(secretKey.getBytes()))
                 .withIssuer(ISSUER)
                 .build();
     }
-    
+
     public static String getToken(String userId) {
-    		Date expires = JwtTokenUtil.getTokenExpiration(expirationTime);
+        Date expires = JwtTokenUtil.getTokenExpiration(expirationTime);
         return JWT.create()
                 .withSubject(userId)
                 .withExpiresAt(expires)
@@ -60,10 +61,10 @@ public class JwtTokenUtil {
                 .withIssuedAt(Date.from(LocalDateTime.now().atZone(ZoneId.systemDefault()).toInstant()))
                 .sign(Algorithm.HMAC512(secretKey.getBytes()));
     }
-    
+
     public static Date getTokenExpiration(int expirationTime) {
-    		Date now = new Date();
-    		return new Date(now.getTime() + expirationTime);
+        Date now = new Date();
+        return new Date(now.getTime() + expirationTime);
     }
 
     public static boolean isValidToken(String token) {
