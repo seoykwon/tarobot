815126f3170faa889f858b30545194bccc225daf
Riis <gunil099@gmail.com>
2025-02-19 15:19:06 +0900

    remove: 폴더 구조 변경

--- frontend/src/components/Sidebar/Sidebar.tsx
+++ frontend/src/components/Sidebar/Sidebar.tsx
@@ -1,10 +1,10 @@
 "use client"
 
 import { useState } from "react"
-import TarotMasterList from "./TarotMasterList"
-import ClientSidebar from "./ClientSidebar"
-import SessionList from "./SessionList"
-import CharacterSelect from "./CharacterSelect"
+import TarotMasterList from "@/components/Sidebar/TarotMasterList"
+import ClientSidebar from "@/components/Sidebar/ClientSidebar"
+import SessionList from "@/components/Sidebar/SessionList"
+import CharacterSelect from "@/components/CharacterSelect"
 
 export default function Sidebar({ isOpen, setIsOpen }: { isOpen: boolean; setIsOpen: (open: boolean) => void }) {
   const [isCharacterSelectOpen, setIsCharacterSelectOpen] = useState(false)
