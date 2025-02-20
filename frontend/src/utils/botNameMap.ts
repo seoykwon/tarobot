938786ab759e422878c0aa9206bb94171ccb4156
Won, Kee-hoon <lasnier@naver.com>
2025-02-21 08:52:35 +0900

    fix: 타로봇 인덱스를 맞추기 위해 next.js와 fast의 인덱스를 수정했습니다.

--- frontend/src/utils/botNameMap.ts
+++ frontend/src/utils/botNameMap.ts
@@ -1,10 +1,10 @@
 // utils/botNameMap.ts
 export const botNameMap: Record<string, string> = {
-    "0": "진이",
-    "1": "범달",
-    "2": "3번",
-    "3": "4번",
-    "4": "5번",
+    "1": "진이",
+    "2": "범달",
+    "3": "3번",
+    "4": "4번",
+    "5": "5번",
   };
   
   export const getBotName = (botId: string | null): string => {
